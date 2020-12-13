9a5c7eaf78c7eacfd45dd11f34d5e218b61ae0a8
ComputerNerd <theprogrammernerd@gmail.com>
2020-12-13 15:22:47 -0600

    Same problem with Widget.cxx. The headers needed to be included in the header.

--- src/Widget.cxx
+++ src/Widget.cxx
@@ -20,7 +20,6 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
 #include <FL/fl_ask.H>
 #include <FL/fl_draw.H>
-#include <FL/Fl_Widget.H>
 
 #include "Bitmap.H"
 #include "Blend.H"
