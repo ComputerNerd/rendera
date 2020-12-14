1f86875470214464f73c194205f57442591ddb0a
ComputerNerd <theprogrammernerd@gmail.com>
2020-12-13 22:32:21 -0600

    Include fl_ask.H which is needed for fl_alert.

--- src/File.cxx
+++ src/File.cxx
@@ -42,6 +42,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 #include <FL/Fl_Group.H>
 #include <FL/Fl_Image.H>
 #include <FL/Fl_Native_File_Chooser.H>
+#include <FL/fl_ask.H>
 
 #include "Bitmap.H"
 #include "Dialog.H"
