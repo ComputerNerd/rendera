5739221bb64dc33d0bb6c5969630b4fd47a5746e
ComputerNerd <theprogrammernerd@gmail.com>
2020-12-13 22:24:35 -0600

    Add  error messages to File.cxx for when the native file chooser returns an error.

--- src/File.cxx
+++ src/File.cxx
@@ -237,6 +237,7 @@ void File::load(Fl_Widget *, void *)
   switch(fc.show())
   {
     case -1:
+      fl_alert("An error occurred while attempting to select a file: %s.", fc.errmsg());
     case 1:
       return;
     default:
@@ -934,6 +935,7 @@ void File::save(Fl_Widget *, void *)
   switch(fc.show())
   {
     case -1:
+      fl_alert("An error occurred while attempting to select a file: %s.", fc.errmsg());
     case 1:
       return;
     default:
@@ -1483,6 +1485,7 @@ void File::loadPalette()
   switch(fc.show())
   {
     case -1:
+      fl_alert("An error occurred while attempting to select a file: %s.", fc.errmsg());
     case 1:
       return;
     default:
@@ -1528,6 +1531,7 @@ void File::savePalette()
   switch(fc.show())
   {
     case -1:
+      fl_alert("An error occurred while attempting to select a file: %s.", fc.errmsg());
     case 1:
       return;
     default:
